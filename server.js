3a890b3215ab77766b95c4d73b16a2ad9311fdce
hasan <hasan@DESKTOP-CJ3QF82>
2024-08-02 00:40:35 +0700

    .

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve HTML form
-app.get('/wasteForm', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
