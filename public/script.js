3353b2691ee08225a516601d2ca159c31da534a8
hasan <hasan@DESKTOP-CJ3QF82>
2024-08-10 21:23:30 +0700

    iii

--- public/script.js
+++ public/script.js
@@ -3,6 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultDiv = document.getElementById('result');
     const downloadButton = document.getElementById('downloadData');
 
+    const shift1PowderWaste = parseFloat(document.getElementById('shift1PowderWaste').value) || 0;
+    const shift1DoughWaste = parseFloat(document.getElementById('shift1DoughWaste').value) || 0;
+    const shift2PowderWaste = parseFloat(document.getElementById('shift2PowderWaste').value) || 0;
+    const shift2DoughWaste = parseFloat(document.getElementById('shift2DoughWaste').value) || 0;
+    const shift3PowderWaste = parseFloat(document.getElementById('shift3PowderWaste').value) || 0;
+    const shift3DoughWaste = parseFloat(document.getElementById('shift3DoughWaste').value) || 0;
+
+    // Calculate total waste for powder and dough
+    const totalPowderWaste = shift1PowderWaste + shift2PowderWaste + shift3PowderWaste;
+    const totalDoughWaste = shift1DoughWaste + shift2DoughWaste + shift3DoughWaste;
+
+    document.getElementById('totalB').value=totalDoughWaste;
+    document.getElementById('totalA').value=totalPowderWaste;
+
     // Handle waste data submission
     wasteForm.addEventListener('submit', async (event) => {
         event.preventDefault(); 
@@ -135,9 +149,6 @@ function calculateWaste() {
     // Calculate total waste for powder and dough
     const totalPowderWaste = shift1PowderWaste + shift2PowderWaste + shift3PowderWaste;
     const totalDoughWaste = shift1DoughWaste + shift2DoughWaste + shift3DoughWaste;
-
-    document.getElementById('totalB').value=totalDoughWaste;
-    document.getElementById('totalA').value=totalPowderWaste;
 
     var tw = new Date(document.getElementById('date').value);
     if (tw.getTimezoneOffset() == 0) (a=tw.getTime() + ( 7 *60*60*1000))
