d184e881e6da34b618bee0bd5cc0af3ac0e2a2a0
hasan <hasan@DESKTOP-CJ3QF82>
2024-08-14 21:35:24 +0700

    test

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/waste', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'waste.html'));
+});
+
 app.post('/submit', async (req, res) => {
   const {
     date,
