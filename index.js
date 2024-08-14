f21604ed12f6ace92f9a44348f69d3744e93286f
hasan <hasan@DESKTOP-CJ3QF82>
2024-08-14 19:48:26 +0700

    up

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/submit', async (req, res) => {
